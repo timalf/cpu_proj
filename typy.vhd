00a08cf7fe4880332621d9f9195c11dd9108791c
timalf <timalf@users.noreply.github.com>
2015-04-28 13:18:21 +0200

    Create typy.vhd

---
+++ typy.vhd
@@ -0,0 +1,42 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    11:36:56 04/28/2015 
+-- Design Name: 
+-- Module Name:    typy - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+package typy is
+ 
+        type rejdane is array(3 downto 0) of std_logic_vector(7 downto 0);
+        type rejIO is array(3 downto 0) of std_logic;
+ 
+end package typy;
+
+package body typy is
+ 
+ 
+end typy;
