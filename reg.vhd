c89d8e60e13793716011e0da28ef6ae36bb68375
timalf <timalf@users.noreply.github.com>
2015-05-12 12:11:11 +0200

    Update reg.vhd

--- reg.vhd
+++ reg.vhd
@@ -33,28 +33,64 @@ use work.typy.ALL;
 --use UNISIM.VComponents.all;
 
 entity reg is
-port(
-  IO: in rejIO; 
-  --regnumb: in std_logic_vector (3 downto 0);
-  input: in rejdane;
-  output: out rejdane
-  );
+  
+  port (
+    CLK	    : in  std_logic;
+    RegWrite : in  std_logic;
+    Rx       : in  std_logic_vector(1 downto 0);
+    wData    : in  std_logic_vector(7 downto 0);
+    
+    R0_out   : out std_logic_vector(7 downto 0);
+    R1_out   : out std_logic_vector(7 downto 0);
+    R2_out   : out std_logic_vector(7 downto 0);
+    R3_out   : out std_logic_vector(7 downto 0)
+   
+    
+    );
+
 end reg;
  
-architecture Behavioral of reg is
-  type regmod is array(0 to 3) of std_logic_vector(7 downto 0);
-  signal rejestry: regmod;
+architecture reg_Arch of reg is
 
-begin
-   regs: for i in 0 to 3 generate
-    process(IO, input)
-    begin
-        if(IO(i) = '1') then
-          rejestry(i) <= input(i);
-			 else
-			 output(i) <= rejestry(i);
-        end if;
-    end process;
-   -- output(i) <= rejestry(i) when IO(i)='0' else input(i);
-  end generate regs;
-end Behavioral;
+  signal R0 : std_logic_vector(7 downto 0) := (others => '0');
+  signal R1 : std_logic_vector(7 downto 0) := (others => '0');
+  signal R2 : std_logic_vector(7 downto 0) := (others => '0');
+  signal R3 : std_logic_vector(7 downto 0) := (others => '0');
+  
+begin  -- reg_Arch
+
+  -- Zapis do rejestru
+  process (CLK)
+  begin 
+    if rising_edge(CLK) then
+      if RegWrite = '1' then
+        case Rx is
+          when "00" => R0 <= wData;
+          when "01" => R1 <= wData;
+          when "10" => R2 <= wData;
+          when "11" => R3 <= wData;
+          when others => null;
+        end case;
+      end if;
+    end if;
+  end process;
+
+
+  -- Odczyt
+  process (CLK)
+  begin 
+    if falling_edge(CLK) then
+		if RegWrite = '0' then
+      case Rx is
+        when "00" => R0_out <= R0;
+        when "01" => R1_out <= R1;
+        when "10" => R2_out <= R2;
+        when "11" => R3_out <= R3;
+        when others => null;
+      end case;
+		end if;
+    end if;
+  end process;
+
+
+end reg_Arch;
